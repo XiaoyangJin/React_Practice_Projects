502a75835e6ce6547a4a22c431bdcee915b851a3
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-10-14 20:39:34 -0400

    design navbar

--- gpt3/src/components/navbar/Navbar.jsx
+++ gpt3/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
 import './navbar.css';
 
 // BEM -> Block Element Modifier
@@ -11,6 +12,17 @@ const Navbar = () => {
                 <div className='gpt3__navbar-links_logo'>
                     <img src={logo} alt="logo" />
                 </div>
+                <div className='gpt3__navbar-links_container'>
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt3">What is GPT?</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="#features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+            </div>
+            <div className='gpt3__navbar-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
             </div>
         </div>
     )
